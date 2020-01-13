29b260a394c94cbe72c6e586accda4f82526ff92
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-13 15:02:44 +0200

    feat(Polyfills): use polyfills

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 class App {
     run = async (name = 'Word') => {
         console.log(`Hello ${name}`);
+        console.log([1, 2, [2, 3]].flat());
     }
 }
 
 const app = new App();
 app.run()
     .then(() => console.log('done'))
-    .catch(() => console.log('Error'))
+    .catch(() => console.log('Error'));
